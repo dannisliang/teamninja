847172d3a37c1d05c07258db99673ddb30cf8c80
JP Berry <nuonical@gmail.com>
2015-04-27 15:12:24 -0400

    Initial time of day functionality for multiplayer.

---
+++ Assets/MultiPlayerCore.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiPlayerCore : MonoBehaviour {
+
+	public bool RaysAdded = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!RaysAdded) {
+
+
+			Camera mainC = Camera.main;
+
+			// Set clear flags to depth only so lighting works properly
+			mainC.clearFlags = CameraClearFlags.Depth;
+
+			// Add flare layer for sun flare to show up
+			GameObject fpCam = GameObject.Find("FPSCamera");
+
+			if(fpCam != null) {
+				fpCam.AddComponent<FlareLayer>();
+			}
+
+
+			RaysAdded = true;
+			Debug.LogWarning("Added Rays");
+		}
+
+
+	}
+}
